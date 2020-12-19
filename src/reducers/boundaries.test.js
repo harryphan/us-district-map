8e3cba433098a88b0ff632db124c1ac853b72384
Harry Phan <harryphan@gmail.com>
2020-12-19 10:53:05 -0500

    loading states

--- src/reducers/boundaries.test.js
+++ src/reducers/boundaries.test.js
@@ -3,21 +3,21 @@ import {LOAD_COUNTIES, LOAD_STATES} from "../constants/action_constants";
 
 describe('boundaries reducer test', () => {
     it('should return the initial state', () =>{
-        const expected={counties: null,states:null}
+        const expected={counties: null,states:null,isLoading:false}
         expect(reducer(undefined, {})).toEqual(expected);
     });
     it('should update state with payload', () =>{
-        const expected={counties: null,states:[{test:'obj'}]};
+        const expected={counties: null,states:[{test:'obj'}],isLoading:false};
         const action={type: LOAD_STATES,payload:[{test:'obj'}]};
         expect(reducer(undefined, action)).toEqual(expected);
     });
     it('should update counties with payload', () =>{
-        const expected={counties: [{test:'obj'}],states:null};
+        const expected={counties: [{test:'obj'}],states:null,isLoading:false};
         const action={type: LOAD_COUNTIES,payload:[{test:'obj'}]};
         expect(reducer(undefined, action)).toEqual(expected);
     });
     it('should do nothing', () =>{
-        const expected={counties:null ,states:null};
+        const expected={counties:null ,states:null,isLoading:false};
         const action={type: 'fadjfla',payload:[{test:'obj'}]};
         expect(reducer(undefined, action)).toEqual(expected);
     });
--- src/reducers/covidData.test.js
+++ src/reducers/covidData.test.js
@@ -3,21 +3,21 @@ import { LOAD_COVID, LOAD_US_COVID} from "../constants/action_constants";
 
 describe('covid data reducer test', () => {
     it('should return the initial state', () =>{
-        const expected={us:[], ma:[]}
+        const expected={us:[], ma:[],isLoading:false}
         expect(reducer(undefined, {})).toEqual(expected);
     });
     it('should update us with payload', () =>{
-        const expected={us:[{ny:2500}], ma:[]}
+        const expected={us:[{ny:2500}], ma:[],isLoading:false}
         const action={type: LOAD_US_COVID,payload:[{ny:2500}]};
         expect(reducer(undefined, action)).toEqual(expected);
     });
     it('should update ma with payload', () =>{
-        const expected={us:[], ma:[{suffolk:20}]}
+        const expected={us:[], ma:[{suffolk:20}],isLoading:false}
         const action={type: LOAD_COVID,payload:[{suffolk:20}]};
         expect(reducer(undefined, action)).toEqual(expected);
     });
     it('should do nothing', () =>{
-        const expected={us:[], ma:[]};
+        const expected={us:[], ma:[],isLoading:false};
         const action={type: 'fadjfla',payload:[{test:'obj'}]};
         expect(reducer(undefined, action)).toEqual(expected);
     });
